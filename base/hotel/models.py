30c5a9a751809553e0735588168d21e48bd62fd9
Ahmed Mostafa <kudo7474@gmail.com>
2024-03-12 13:39:38 +0200

    Edit Model

--- base/hotel/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-# Create your models here.
-class Hotel(models.Model):
-    name = models.CharField(max_length=200)  
-    address = models.TextField(null=True,blank=True)
-    def __str__(self):
-        return self.name 
-    
