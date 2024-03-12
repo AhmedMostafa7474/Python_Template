496ad7b878ab7dc18623c8b293780fe5e4074fd8
Ahmed Mostafa <36231373+AhmedMostafa7474@users.noreply.github.com>
2024-03-12 10:58:53 +0200

    Update settings.py

--- studypython/settings.py
+++ studypython/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'studypython.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_pycopg2',
+        'NAME': 'djangoprod',
+        'USER' : 'admin',
+        'PASSWORD' : 'password',
+        'HOST' : 'localhost',
+        'PORT' :''
     }
 }
 
