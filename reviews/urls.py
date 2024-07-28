ae93fedba0d60abafb7cc068abdc62059203c286
Ahmed Mostafa <kudo7474@gmail.com>
2024-07-28 16:38:08 +0300

    Add check Survay Basket

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.SurveyListCreate.as_view(),name="survay-create"),
     path('checksurvey/',views.SurvayValidationView.as_view(),name="survay-create"),
+    path('basketchecksurvey/',views.SurvayBasketValidationView.as_view(),name="survay-basket"),
 
 ]
