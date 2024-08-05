ff9295b5e3ebf103e90b61a4f0075265f8a06966
Ahmed Mostafa <kudo7474@gmail.com>
2024-08-05 16:51:00 +0300

    Edit Model

--- reviews/models.py
+++ reviews/models.py
@@ -73,4 +73,4 @@ class Survey(models.Model):
     completed = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.username + " Survey"
+        return self.id + " Survey"
